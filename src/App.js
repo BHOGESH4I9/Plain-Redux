fbd891143982cd3c66a06fe464c2f86c75e377ec
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-25 02:07:57 -0400

    new file store is created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import './redux';
+import './redux/store'
 import Form from './components/Form';
 import Account from './components/Account';
 
 function App() {
   return (
     <div className="min-vh-100 d-flex justify-content-center align-items-center bg-light">
-      <div className="p-4 rounded shadow bg-white text-center" style={{ width: '500px' }}>
+      <div className="p-4 rounded shadow bg-white text-center" style={{ width: '800px' }}>
         <h2 className="mb-4 fw-bold text-primary">Bank Account Dashboard</h2>
         <Form />
         <hr />
