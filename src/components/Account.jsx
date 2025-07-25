01462c773efa62b991d7262c40e7b9dc14157094
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-25 03:14:48 -0400

    Create a new table transaction details

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -20,11 +20,35 @@ const Account = () => {
           </thead>
           <tbody>
             <tr>
-              <td>{data.name}</td>
-              <td>{data.accountNumber}</td>
-              <td>{data.mobile}</td>
-              <td>₹{data.balance}</td>
+              <td>{data.account.name}</td>
+              <td>{data.account.accountNumber}</td>
+              <td>{data.account.mobile}</td>
+              <td>₹{data.account.balance}</td>
             </tr>
+          </tbody>
+        </table>
+
+        <h3 className="text-success mb-3">Transaction Details</h3>
+        <table className="table table-bordered table-striped table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th>Id</th>
+              <th>Amount</th>
+              <th>Balance Amount</th>
+              <th>Type</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.transactions.map((txn, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{txn.amount}</td>
+                <td>{txn.balance}</td>
+                <td>{txn.type}</td>
+                <td>{new Date(txn.date).toLocaleString()}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
