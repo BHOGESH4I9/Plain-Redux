3f6c771eb48555fd8afe744040a05eb195a36818
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-25 02:12:35 -0400

    new store props was given to react-redux provider

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import redux from './redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={redux}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
